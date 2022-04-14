132f14b6bda09a1ae403c0f9a63d30bbfa346b28
Folkert van Verseveld <folkert.van.verseveld@gmail.com>
2022-04-14 10:08:53 +0200

    add pwm interpolate

--- Test-libraries/main.cpp
+++ Test-libraries/main.cpp
@@ -1,9 +1,28 @@
 #include "mbed.h"
 #include "board_freedom.h"
-// main() runs in its own thread in the OS
+
+#define PWM_MIN 820
+#define PWM_MAX 1700
+
+// 860, 1720
 int main()
 {
+    unsigned int pwm_min = PWM_MIN;
+
     board_init();
-    pwm3.write (10);
-    pwm3.period_ms(10);
+    pwm0.period_ms(20);
+
+    while(true)
+    {
+        pwm0.pulsewidth_us(pwm_min);
+        ThisThread::sleep_for(1s);
+
+        for (int i = 0; i < 100; i++)
+        {
+            pwm0.pulsewidth_us(pwm_min + i * (PWM_MAX - PWM_MIN) / 100);
+            ThisThread::sleep_for(40ms);
+        }
+
+        ThisThread::sleep_for(1s);
+    }
 }
