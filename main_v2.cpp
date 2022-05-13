786773b9a27f9a52fb10008491f11951e400e433
Qiuyang Fu <47096861+qyfHalLu@users.noreply.github.com>
2022-05-13 20:58:22 +0200

    update

--- main_v2.cpp
+++ main_v2.cpp
@@ -6,9 +6,14 @@
 // GND goes to pin GND of the mbed board
 // 5V goes to pin 5V from Pololu robot
 M3pi robot; //create an object of type  M3pi
-
+DigitalOut redled(p20);
+DigitalOut greenled(p20);
+//DigitalOut greenled(p
 Ping Pinger(p11);
 int range;
+
+AnalogIn passive_light(p20);
+
 
 // int main() {
 //    int range;
@@ -29,21 +34,25 @@ int main() {
     pc.printf("Finished calibration!\r\n");
     while (1) {
         int sensors[5];
-        robot.calibrated_sensors(sensors);  //5 values are read in a 
-vector
-        pc.printf("sensor values: %d; %d; %d; %d; %d;\r\n", sensors[0], 
-sensors[1],sensors[2], sensors[3], sensors[4]);
-
+        robot.calibrated_sensors(sensors);  //5 values are read in a vector
+        pc.printf("sensor values: %d; %d; %d; %d; %d;\r\n", sensors[0], sensors[1],sensors[2], sensors[3], sensors[4]);
+        greenled = 1;
+        redled = 1;
         //if((sensors[2]>800)){ //straight
 //           robot.activate_motor (0,0.2) ; 
 //           robot.activate_motor (1,0.2) ; 
 //       }
+        
+
         if( sensors[1]>150 ){ // turn left
             robot.activate_motor (0,0) ; 
             robot.activate_motor (1,0.3) ; 
             }
         else if (sensors[0]>150 && sensors[0]<600) {
             // sharp left
+            robot.activate_motor (0,-0.15) ; 
+            robot.activate_motor (1,-0.15) ;
+            wait_ms(200);
             robot.activate_motor (0,-0.05) ; 
             robot.activate_motor (1,0.3) ; 
             } 
@@ -53,22 +62,23 @@ sensors[1],sensors[2], sensors[3], sensors[4]);
         }
         else if (sensors[4]>150 && sensors[4]<600) { 
         // sharp right
+            robot.activate_motor (0,-0.15) ; 
+            robot.activate_motor (1,-0.15) ;
+            wait_ms(200);
             robot.activate_motor (0,0.3) ; 
             robot.activate_motor (1,-0.05) ; 
+
             }
             
             
         Pinger.Send();
-        wait_ms(15);
+        wait_ms(0);
         range = Pinger.Read_cm();
         pc.printf("range: %d\r\n", range);
 
-        if((sensors[0]<500 && sensors[1]<500 && sensors[2]<500 && 
-sensors[3]<500 && sensors[4]<500)){
-            robot.activate_motor (0,0) ; // drive left motor 1/10 max 
-speed forward
-            robot.activate_motor (1,0) ; // drive right motor 0/10 max 
-speed forward
+        if( (sensors[0]<200 && sensors[1]<200 && sensors[2]<200 && sensors[3]<200 && sensors[4]<200)){
+            robot.activate_motor (0,0) ; // stop
+            robot.activate_motor (1,0) ; // stop
         }
         //wait_ms(1000);
     }
